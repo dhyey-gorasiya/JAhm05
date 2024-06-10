1a6b3987dddbed39018bfb61d2c3e7a0aa2398cd
Dhyey Gorasiya <dhyeygorasiya@gmail.com>
2024-06-10 12:41:27 +0530

    Add files via upload

---
+++ Geometry_Demo/Triangle.java
@@ -0,0 +1,22 @@
+package Geometry_Demo;
+
+public class Triangle {
+    private double A;
+    private double B;
+    private double C;
+
+    public Triangle(double A, double B, double C) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+    }
+
+    public double getArea() {
+        double s = (A + B + C) / 2;
+        return Math.sqrt(s * (s - A) * (s - B) * (s - C));
+    }
+
+    public double getPerimeter() {
+        return A + B + C;
+    }
+}
